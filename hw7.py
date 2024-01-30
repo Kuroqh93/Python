4bfb4fde623399a07eb5c5f0a2e47db9234f5778
Kuroqh93 <kuro_qh93@student.itstep.org>
2024-01-30 15:47:56 +0200

    hw7

---
+++ hw7.py
@@ -0,0 +1,19 @@
+class BluetoothDevice:
+    def __init__(self, brand, model, max_volume, bass_boost):
+        self.brand = brand
+        self.model = model
+        self.max_volume = max_volume
+        self.bass_boost = bass_boost
+        self.connected_device = None
+
+    def connect(self, other_device):
+        self.connected_device = other_device
+
+blueD1 = BluetoothDevice("JBL", "partybox310", 1000, "yes")
+blueD2 = BluetoothDevice("JBL", "Charge4", 300, "no")
+
+blueD1.connect(blueD2)
+if blueD1.connected_device:
+    print(blueD1.connected_device.max_volume)
+else:
+    print("No connected device.")
